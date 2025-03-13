14898a3974b9a1788e235fe568b05811c2e58050
felmanc <felmanc@gmail.com>
2025-03-13 16:52:33 -0300

    Ajuste de PointOfInterestRepository

--- code/pointsofinterest/src/main/java/br/com/felmanc/pointsofinterest/repositories/PointOfInterestRepository.java
+++ code/pointsofinterest/src/main/java/br/com/felmanc/pointsofinterest/repositories/PointOfInterestRepository.java
@@ -11,12 +11,7 @@ import br.com.felmanc.pointsofinterest.entities.PointOfInterestEntity;
 
 @Repository
 public interface PointOfInterestRepository extends JpaRepository<PointOfInterestEntity, Long> {
-/*
-    @Query("SELECT p FROM PointOfInterestEntity p WHERE SQRT(POW(p.x - :xReferencia, 2) + POW(p.y - :yReferencia, 2)) <= :dMax")
-    List<PointOfInterestEntity> findWithinDistance(@Param("xReferencia") Long xReferencia,
-                                                   @Param("yReferencia") Long yReferencia,
-                                                   @Param("dMax") Double dMax);
-*/
+
     @Query(value = "SELECT * FROM point_of_interest p WHERE SQRT(POW(p.x - :xReferencia, 2) + POW(p.y - :yReferencia, 2)) <= :dMax", nativeQuery = true)
     List<PointOfInterestEntity> findWithinDistance(@Param("xReferencia") Long xReferencia,
                                                    @Param("yReferencia") Long yReferencia,
