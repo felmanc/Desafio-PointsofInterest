a348b4e33e860a7a7ae2d6e38ccb2748b4e79c8e
felmanc <felmanc@gmail.com>
2025-03-13 18:42:45 -0300

    Ajuste de configuração

--- code/pointsofinterest/src/main/java/br/com/felmanc/pointsofinterest/PointsofinterestApplication.java
+++ code/pointsofinterest/src/main/java/br/com/felmanc/pointsofinterest/PointsofinterestApplication.java
@@ -3,7 +3,7 @@ package br.com.felmanc.pointsofinterest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "com.felmanc.pointsofinterest")
+@SpringBootApplication(scanBasePackages = "br.com.felmanc.pointsofinterest")
 public class PointsofinterestApplication {
 
 	public static void main(String[] args) {
