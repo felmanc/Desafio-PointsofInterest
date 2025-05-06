b1f5c5301637d6ecb5f9c8965b169b8154abed0c
felmanc <felmanc@gmail.com>
2025-05-06 14:19:01 -0300

    Correção de vulnerabilidades

--- code/pointsofinterest/src/main/java/br/com/felmanc/pointsofinterest/exceptions/GlobalExceptionHandler.java
+++ code/pointsofinterest/src/main/java/br/com/felmanc/pointsofinterest/exceptions/GlobalExceptionHandler.java
@@ -7,7 +7,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
